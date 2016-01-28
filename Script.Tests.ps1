9ebdfb6c4aa7847aaa6e3d74f53feb0d009963ad
Stijn Callebaut <stijnc@fooji.net>
2016-01-28 21:04:59 +0100

    created script.Tests.ps1

---
+++ Script.Tests.ps1
@@ -0,0 +1,9 @@
+#Requires -Modules Pester
+<#
+.SYNOPSIS
+    Tests a script
+.EXAMPLE
+    Invoke-Pester 
+.NOTES
+    checks if a script meets the generic conditions to be published on the PSGallery
+#>
